1b61a6bf7a7650764629f7999e20d9a0ff612428
Vitor Nascimento <bielvitor2008@hotmail.com>
2021-02-23 18:03:50 -0300

    Importado e corrigido os layouts.

---
+++ app/src/main/java/me/vitornascimento/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,14 @@
+package me.vitornascimento.financask.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.vitornascimento.financask.R
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+
+}
