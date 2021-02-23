c42ec24ace74cccfc1bcc7ea115788fd9c06aae6
Vitor Nascimento <bielvitor2008@hotmail.com>
2021-02-23 19:18:35 -0300

    Implementando BaseAdapter.

--- app/src/main/java/me/vitornascimento/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/me/vitornascimento/financask/ui/activity/ListaTransacoesActivity.kt
@@ -3,6 +3,7 @@ package me.vitornascimento.financask.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import me.vitornascimento.financask.databinding.ActivityListaTransacoesBinding
+import me.vitornascimento.financask.ui.adapter.ListaTransacoesAdapter
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
@@ -13,6 +14,11 @@ class ListaTransacoesActivity : AppCompatActivity() {
         binding = ActivityListaTransacoesBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val transacoes = listOf("Teste 1", "Teste 2")
+        val adapter = ListaTransacoesAdapter(transacoes, this)
+        binding.listaTransacoesListview.adapter = adapter
+
     }
 
 }
---
+++ app/src/main/java/me/vitornascimento/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -0,0 +1,31 @@
+package me.vitornascimento.financask.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import me.vitornascimento.financask.R
+
+class ListaTransacoesAdapter(
+    private val transacoes: List<String>,
+    private val context: Context
+) : BaseAdapter() {
+
+
+    override fun getCount(): Int {
+        return transacoes.size
+    }
+
+    override fun getItem(position: Int): String {
+        return transacoes[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        return LayoutInflater.from(context).inflate(R.layout.transacao_item, parent, false)
+    }
+}
