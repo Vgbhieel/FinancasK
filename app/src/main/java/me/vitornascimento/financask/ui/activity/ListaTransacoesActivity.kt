1e083e0cabce1936110c1ca752abc0d17d8f132f
Vitor Nascimento <bielvitor2008@hotmail.com>
2021-03-08 19:51:17 -0300

    Implementa listener ao positiveButton do AlertDialog.

--- app/src/main/java/me/vitornascimento/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/me/vitornascimento/financask/ui/activity/ListaTransacoesActivity.kt
@@ -5,8 +5,8 @@ import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.EditText
-import android.widget.RelativeLayout
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import me.vitornascimento.financask.R
 import me.vitornascimento.financask.databinding.ActivityListaTransacoesBinding
@@ -16,9 +16,12 @@ import me.vitornascimento.financask.model.Transacao
 import me.vitornascimento.financask.ui.ResumoView
 import me.vitornascimento.financask.ui.adapter.ListaTransacoesAdapter
 import java.math.BigDecimal
+import java.text.SimpleDateFormat
 import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
+
+    private val transacoes: MutableList<Transacao> = mutableListOf()
 
     lateinit var binding: ActivityListaTransacoesBinding
 
@@ -28,28 +31,68 @@ class ListaTransacoesActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val transacoes: List<Transacao> = transacoesDeExemplo()
+        configuraResumoView()
 
-        configuraResumoView(view, transacoes)
-
-        configuraListView(transacoes)
+        configuraListView()
 
         binding.listaTransacoesAdicionaReceita.setOnClickListener {
 
             val viewDialog = layoutInflater.inflate(R.layout.form_transacao, null)
+
+            val formDataTransacao = viewDialog
+                    .findViewById<EditText>(R.id.form_transacao_data)
+            val formCategoriaTransacao = viewDialog
+                    .findViewById<Spinner>(R.id.form_transacao_categoria)
+            val formValorTransacao = viewDialog
+                    .findViewById<EditText>(R.id.form_transacao_valor)
+
             AlertDialog
                     .Builder(this)
                     .setTitle(R.string.adiciona_receita)
                     .setView(viewDialog)
-                    .setPositiveButton("Adicionar", null)
+                    .setPositiveButton("Adicionar") { _, _ ->
+
+                        val valorEmString = formValorTransacao
+                                .text.toString()
+
+                        val dataEmString = formDataTransacao
+                                .text.toString()
+
+                        val categoriaEmString = formCategoriaTransacao
+                                .selectedItem.toString()
+
+                        val valorEmBigDecimal = try {
+                            BigDecimal(valorEmString)
+                        } catch (e: NumberFormatException) {
+                            Toast
+                                    .makeText(this, "Erro ao converter o valor.", Toast.LENGTH_LONG)
+                                    .show()
+
+                            BigDecimal.ZERO
+                        }
+
+                        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR"))
+                        val dataParaDate = sdf.parse(dataEmString) as Date
+                        val dataParaCalendar = Calendar.getInstance().apply {
+                            time = dataParaDate
+                        }
+
+                        val transacaoCriada = Transacao(tipo = TipoTransacao.RECEITA,
+                                valor = valorEmBigDecimal,
+                                data = dataParaCalendar,
+                                categoria = categoriaEmString)
+
+                        adicionaTransacao(transacaoCriada)
+                        binding.listaTransacoesAdicionaMenu.close(true)
+                    }
                     .setNegativeButton("Cancelar", null)
                     .show()
 
-            val formDataTransacao = viewDialog.findViewById<EditText>(R.id.form_transacao_data)
+
             val hoje = Calendar.getInstance()
-            val dia = 26
-            val mes = 2
-            val ano = 2021
+            val dia = hoje.get(Calendar.DAY_OF_MONTH)
+            val mes = hoje.get(Calendar.MONTH)
+            val ano = hoje.get(Calendar.YEAR)
             formDataTransacao.setText(hoje.formataParaBrasileiro())
             formDataTransacao.setOnClickListener {
                 DatePickerDialog(this,
@@ -64,8 +107,6 @@ class ListaTransacoesActivity : AppCompatActivity() {
                         .show()
             }
 
-            val formCategoriaTransacao = viewDialog
-                    .findViewById<Spinner>(R.id.form_transacao_categoria)
             val spinnerAdapter = ArrayAdapter
                     .createFromResource(this,
                             R.array.categorias_de_receita,
@@ -74,29 +115,20 @@ class ListaTransacoesActivity : AppCompatActivity() {
         }
     }
 
-    private fun configuraResumoView(view: RelativeLayout, transacoes: List<Transacao>) {
-        val resumoView = ResumoView(view, transacoes)
+    private fun adicionaTransacao(transacao: Transacao) {
+        transacoes.add(transacao)
+        configuraListView()
+        configuraResumoView()
+    }
+
+    private fun configuraResumoView() {
+        val resumoView = ResumoView(binding.root, transacoes)
         resumoView.atualiza()
     }
 
-    private fun configuraListView(transacoes: List<Transacao>) {
+    private fun configuraListView() {
         val adapter = ListaTransacoesAdapter(transacoes)
         binding.listaTransacoesListview.adapter = adapter
     }
-
-    private fun transacoesDeExemplo() = listOf(
-            Transacao(
-                    valor = BigDecimal(100.0), tipo = TipoTransacao.DESPESA
-            ),
-            Transacao(
-                    BigDecimal(100.0), "Economia", TipoTransacao.RECEITA
-            ),
-            Transacao(
-                    BigDecimal(100.0), tipo = TipoTransacao.DESPESA
-            ),
-            Transacao(
-                    BigDecimal(100.0), categoria = "Salário", tipo = TipoTransacao.RECEITA
-            )
-    )
 
 }
